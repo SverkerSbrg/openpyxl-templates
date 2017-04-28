56ac48678831694c2435f34d4ef3caccdf3fbc4a
sverkersbrg@gmail.com <sverkersbrg@gmail.com>
2017-04-28 16:17:23 +0200

    Build instructions

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ import sys
 # if (sys.version_info[0], sys.version_info[1]) < (3, 2):
 #     install_requires.append('futures>=2.1.3')
 
+# To create destribution
+# pip install wheel
+# Use cmd
+# python setup.py bdist_wheel
+# twine upload dist/openpyxl_templates-X.X.X-py2.py3-none-any.whl
+
 setup(
     name='openpyxl-templates',
     version='0.1.5',
